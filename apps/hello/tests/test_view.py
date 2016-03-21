8893b1d9d58c1417df79dc857801cd3e0bb383ab
Алексей <aleks.woronow@yandex.ru>
2016-03-21 20:55:41 +0000

    ticket:1 added test for view that return hard-coded data

---
+++ apps/hello/tests/test_view.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from django.http import HttpRequest
+
+from ..views import home_page
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_home_page_view(self):
+        """Test view home_page"""
+        # Create an instance of a GET request.
+        request = self.factory.get('/')
+
+        # Test home_page() as if it were deployed at /
+        response = home_page(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Test home.html was used in rendering response
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        """Test home page"""
+
+        response = self.client.get(reverse('hello:home'))
+
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response,
+                            '<h1>42 Coffee Cups Test Assignment</h1>',
+                            html=True)
+        self.assertContains(response, 'Aleks')
+        self.assertContains(response, 'Woronow')
+        self.assertContains(response, 'Feb. 25, 2016')
+        self.assertContains(response, 'aleks.woronow@yandex.ru')
+        self.assertContains(response, 'aleksw@42cc.co')
+        self.assertContains(response, 'aleks_woronow')
+        self.assertContains(response, 'I was born ...')
