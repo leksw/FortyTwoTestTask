345e9e4db0f6e0594bbd37a8cef84eba7b23e483
Алексей <aleks.woronow@yandex.ru>
2016-03-22 20:58:01 +0000

    ticket:3 added test request_ajax view

--- apps/hello/tests/test_view.py
+++ apps/hello/tests/test_view.py
@@ -120,3 +120,60 @@ class RequestViewTest(TestCase):
         self.assertIn('Date', response.content)
         self.assertIn('/', response.content)
         self.assertIn('GET', response.content)
+
+class RequestAjaxTest(TestCase):
+    def test_request_ajax_view(self):
+        """Test request ajax view"""
+        self.client.get(reverse('hello:home'))
+        response = self.client.get(reverse('hello:requests_ajax'),
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        self.assertIn('method', response.content)
+        self.assertIn('GET', response.content)
+        self.assertIn('path', response.content)
+        self.assertIn('/', response.content)
+
+    def test_request_ajax_content_empty_db(self):
+        """
+        Test check that request_ajax view returns
+        empty response when transition to request_view page.
+        """
+
+        response = self.client.get(reverse('hello:requests_ajax'),
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        # check that db is empty
+        request_store_count = RequestStore.objects.count()
+        self.assertGreaterEqual(request_store_count, 0)
+        # check response is empty too
+        self.assertIn('0', response.content)
+        self.assertIn('[]', response.content)
+
+    def test_request_ajax_content_record_db_more_10(self):
+        """
+        Test check that request_ajax view returns 10 objects
+        when in db more than 10 records.
+        """
+
+        # create 15 records to db
+        for i in range(1, 15):
+            path = '/test%s' % i
+            method = 'GET'
+            RequestStore.objects.create(path=path, method=method, priority=i)
+
+        self.client.get(reverse('hello:home'))
+        request_store_count = RequestStore.objects.count()
+        self.assertGreaterEqual(request_store_count, 1)
+
+        # check number of objects in db
+        req_list = RequestStore.objects.count()
+        self.assertEqual(req_list, i+1)
+
+        # check that 10 objects in response
+        response = self.client.get(reverse('hello:requests_ajax'),
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(10, response.content.count('pk'))
+        self.assertEqual(10, response.content.count('GET'))
+        self.assertNotIn('/test3', response.content)
+        self.assertNotIn('/test4', response.content)
+        self.assertIn('/test6', response.content)
+        self.assertIn('/', response.content)
