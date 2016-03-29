99bb5dafba66de21c31a1426302d9419e7d3343e
Алексей <aleks.woronow@yandex.ru>
2016-03-29 23:27:50 +0000

    ticket:8 correct output command showmodel

--- apps/hello/management/commands/showmodels.py
+++ apps/hello/management/commands/showmodels.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import logging
-
 from django.core.management.base import NoArgsCommand
 from django.contrib.contenttypes.models import ContentType
 
